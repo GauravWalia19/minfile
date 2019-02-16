e9d2489f7ca9ec798c1d139a230843f28450b16e
GauravWalia19 <gauravwalia019@gmail.com>
2019-02-16 23:18:02 +0530

    added trav.bash for travis

---
+++ trav.bash
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "building..."
+
+cd src                                          # navigate to source directory
+gcc Main.c -o minfile                           # produce the main output file minfile
