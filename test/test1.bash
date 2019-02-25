3c69461d53fa9f3198ed89a71632ab04de20c4f3
GauravWalia19 <gauravwalia019@gmail.com>
2019-02-26 01:36:10 +0530

    testing

---
+++ test/test1.bash
@@ -0,0 +1,13 @@
+echo ""
+echo "TEST 1"
+
+./minfile test.css
+
+echo "OUTPUT:"
+cat test.min.css
+
+rm test.min.css
+
+echo ""
+echo "============ test completed ============"
+echo ""
--- trav.bash
+++ trav.bash
@@ -5,4 +5,7 @@ echo "building..."
 cd src                                          # navigate to source directory
 gcc Main.c -o minfile                           # produce the main output file minfile
 
+cd ..
+cd test
+bash test1.bash
 echo "Build completed"
